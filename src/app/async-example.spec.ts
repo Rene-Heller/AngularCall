3b7c8cde42c825df5edcf8b4c355b97fdf71d1d4
Rene-Heller <rene-heller@developerakademie.com>
2025-05-28 07:04:27 +0200

    added asyc-excample.spec

---
+++ src/app/async-example.spec.ts
@@ -0,0 +1,17 @@
+xdescribe('Async Testing Examples', () => {
+
+  it('Asynchronous test example with Jasmine done()', () => { });
+
+
+  it('Asynchronous test example - setTimeout()', () => { });
+
+  ////////////////////////////////Micro-tasks///////////////////////////////////////
+
+
+  it('Asynchronous test example - plain Promise', () => { });
+
+
+  it('Asynchronous test example - Promises + setTimeout()', () => { });
+
+  it('Asynchronous test example - Observables', () => { });
+});
