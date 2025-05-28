4907910ad1be43921b61640dc3a265419fedf731
Rene-Heller <rene-heller@developerakademie.com>
2025-05-28 08:48:04 +0200

    added dog-data

---
+++ src/app/fetch-example/dog-data.ts
@@ -0,0 +1,17 @@
+export interface Dog {
+  message: string;
+};
+
+export const dogData = {
+  "message":
+    [
+      "afghan",
+      "basset",
+      "blood",
+      "english",
+      "ibizan",
+      "plott",
+      "walker"
+    ],
+  "status": "success"
+}
