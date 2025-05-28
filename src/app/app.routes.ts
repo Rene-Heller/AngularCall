74d9cf9c1aa723778b83d0bd81687e5b56bdeb59
Rene-Heller <rene-heller@developerakademie.com>
2025-05-28 08:47:37 +0200

    updated config and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ServiceContainerComponent } from './service-example/service-container/service-container.component';
+import { DogComponent } from './fetch-example/dog-component/dog.component';
 
 
 export const routes: Routes = [
   { path: 'landing-page', component: LandingPageComponent },
   { path: 'service-test', component: ServiceContainerComponent },
+  { path: 'dogs', component: DogComponent },
   { path: '**', redirectTo: 'landing-page' },
-  // { path: 'component', component:ComponentExampleComponent },
   // { path: 'service', component:ServiceExampleComponent },
   // { path: 'directive', component:DirectiveExampleComponent },
   // { path: 'pipe', component:PipeExampleComponent },
